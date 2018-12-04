380c8e59429d6e7ce76d787cbec45625c9671360
Hugo <hugodahl99@hotmail.com>
2018-12-04 18:16:33 +0100

    First Commit

---
+++ PHP/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="theme.css">
+    <script src="JS/script.js"></script>
+    <title>TNMK30 - Projektarbete</title>
+    
+</head>
+<body>
+
+</body>
+</html>
