d4fdcf1c1bb317bd33bdc48ba461b9ae53ee95f8
CrispyFruits <hugodahl99@hotmail.com>
2018-12-10 17:21:23 +0100

    Fixed nav

--- PHP/index.php
+++ PHP/index.php
@@ -1,15 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="theme.css">
-    <script src="JS/script.js"></script>
-    <title>TNMK30 - Projektarbete</title>
-    
-</head>
-<body>
+  <?php include 'menu.txt'; ?>
+  <main>
+
+  </main>
 
 </body>
+
 </html>
+
+<!-- <div>Font made from <a href="http://www.onlinewebfonts.com">oNline Web Fonts</a>is licensed by CC BY 3.0</div> -->
